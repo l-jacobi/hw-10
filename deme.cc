6551471f64fc8d61f63d5bad1f0b71a5fdb796ec
l-jacobi <eljacobi@squid-laptop.localdomain>
2021-12-02 15:15:52 -0800

    some fuckery with the population comparisons, other changes

--- deme.cc
+++ deme.cc
@@ -8,13 +8,19 @@
 #include <algorithm>
 //#include <numeric>
 
+Chromosome* mut_decider(std::default_random_engine& generator, Chromosome* chromosome_ptr, double mut_rate){
+	if(generator.max() / generator() < mut_rate){
+		chromosome_ptr->mutate();
+	}
+	return chromosome_ptr;
+}
+
+//Deme Members
+
 // Generate a Deme of the specified size with all-random chromosomes.
 // Also receives a mutation rate in the range [0-1].
-
-Chromosome* mut_decider(std::default_random_engine& generator, Chromosome* chromosome_ptr, double mut_rate);
-
 Deme::Deme(const Cities* cities_ptr, unsigned pop_size, double mut_rate){
-	for(int i = 0; i < int(pop_size); ++i){
+	for(unsigned i = 0; i < pop_size; ++i){
 		Chromosome* chromosome_ptr = new Chromosome(cities_ptr);
 		pop_.push_back(chromosome_ptr);
 		//pop_.push_back(std::make_shared<Chromosome>(cities_ptr));
@@ -37,7 +43,7 @@ Deme::~Deme(){
 // a new pair of chromosomes, which are stored in the Deme.
 // After we've generated pop_size new chromosomes, we delete all the old ones.
 void Deme::compute_next_generation(){
-	for(int i = 0; i <= int(pop_.size()) / 2; ++i){
+	for(auto i = 0; i <= pop_.size() / 2; ++i){
 		Chromosome* parent_1 = mut_decider(generator_, select_parent(), mut_rate_);
 		Chromosome* parent_2 = mut_decider(generator_, select_parent(), mut_rate_);
 
@@ -49,31 +55,24 @@ void Deme::compute_next_generation(){
 	}
 }
 
-Chromosome* mut_decider(std::default_random_engine& generator, Chromosome* chromosome_ptr, double mut_rate){
-	if(generator.max() / generator() < mut_rate){
-		chromosome_ptr->mutate();
-	}
-	return chromosome_ptr;
-}
-
 // Return a copy of the chromosome with the highest fitness.
 // ^ This is literally impossible, the chromosome.hh implementation we were given deletes the copy and assignment constructor. I'm going with the moodle instructions, which say to return a pointer to the best chromosome.
 const Chromosome* Deme::get_best() const{
 	//return std::max_element(/*construct an array of the get_fitness of eawch element of pop_*/)
 	//not sure if this ^ would be faster or slower; I'll compare later if I have time
 
-	Chromosome* ret = pop_[0];
+	Chromosome* best = pop_[0];
 	//for(chromosome_ptr_t chromosome_ptr : pop_){ //more spicy code i can't use
 	if(pop_.size() > 1){
-		double current_fitness = ret->get_fitness();	//saves on computing the fitness of the current best option every time they're compared later
-		for(int i = 1; i < int(pop_.size()); ++i){	//is it faster to assign pop_.size() to a variable so i don't have to access it like this? i checked the implementation for vectors and it's calculated by subtraction of the begin and end iterators
-			if(pop_[i]->get_fitness() > current_fitness){
-				ret = pop_[i];
-				current_fitness = ret->get_fitness();
+		double best_fitness = best->get_fitness();	//saves on computing the fitness of the current best option every time they're compared later
+		for(auto i = 1; i < pop_.size(); ++i){	//is it faster to assign pop_.size() to a variable so i don't have to access it like this? i checked the implementation for vectors and it's calculated by subtraction of the begin and end iterators
+			if(pop_[i]->get_fitness() > best_fitness){
+				best = pop_[i];
+				best_fitness = best->get_fitness();
 			}
 		}
 	}
-	return ret;
+	return best;
 }
 
 // Randomly select a chromosome in the population based on fitness and
@@ -92,5 +91,5 @@ Chromosome* Deme::select_parent(){
 		++i;
 	}
 	++i;	// sets i to the first chromosome with a fitness space larger than the random number
-	return pop_[i]; 
+	return pop_[i];
 }
